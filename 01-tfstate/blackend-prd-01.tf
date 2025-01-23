fc4c6592b278c077d4e6749be0ab5548beb8cb06
ivan.silvio <ivan.sl@hotmail.com>
2025-01-23 02:27:54 -0300

    update

--- 01-tfstate/blackend-prd-01.tf
+++ 01-tfstate/blackend-prd-01.tf
@@ -1,8 +1,7 @@
-
 provider "aws" {
   region  = "us-east-1"
 }
-
+/*
 terraform {
   required_providers {
     aws = {
@@ -11,16 +10,16 @@ terraform {
     }
   }
 }
-
+*/
 
 terraform {
  backend "s3" {
-    bucket         = "labchallenge-pipeline-state"
-    key            = "state/s3_kms_dynamodb.tfstate"
-    region         = "us-east-1"
+    bucket         = ""
+    key            = ""
+    region         = ""
     encrypt        = true
-    kms_key_id     = "alias/labchallenge-pipeline-state"
-    dynamodb_table = "labchallenge-pipeline-state"
+    kms_key_id     = ""
+    dynamodb_table = ""
  }
 }
 
