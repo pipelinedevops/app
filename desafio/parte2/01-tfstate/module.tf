6f7c32cd00922803de680277c988a7989342a4d5
ivan.silvio <ivan.sl@hotmail.com>
2025-01-28 17:56:17 -0300

    fix repo

--- desafio/parte2/01-tfstate/module.tf
+++
@@ -1,7 +0,0 @@
-
-module "tfstate_bucket" {
-  source      = "./tfstate-bucket"
-  bucket_prefix = var.bucketprefix
-  org_id = var.orgid
-  tags = var.tags
-}
--- desafio/parte2/01-tfstate/tfstate-bucket/data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_caller_identity" "current" {}
-#data "aws_organizations_organization" "this" {}
-data "aws_region" "current" {}
-data "aws_canonical_user_id" "current" {}
